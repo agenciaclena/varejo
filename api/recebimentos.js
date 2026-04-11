d9e2d5187942e32d56f8173bb5e874d1f58d20c8
agenciaclena <softclena@gmail.com>
2026-04-11 10:52:39 -0300

    Update recebimentos.js

--- api/recebimentos.js
+++ api/recebimentos.js
@@ -161,7 +161,7 @@ export default async function handler(req, res){
       pagina++
       await new Promise(r => setTimeout(r, 120))
 
-      if(pagina > 50){
+      if(pagina > 5){
         console.log("⛔ SEGURANÇA LOOP")
         break
       }
